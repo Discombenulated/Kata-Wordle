59c986592a9d5adf7d47affc76798b7b55a05c01
Ben Noble <ben@benshouse.net>
2022-05-12 16:29:37 +0100

    [BN] More readable as Equals?

--- Wordle/Word.cs
+++ Wordle/Word.cs
@@ -23,7 +23,7 @@ public class Word
 
     private string CheckLetter(Word other, int i)
     {
-        if (this.word[i] == other.word[i]) return CORRECT_LETTER_CORRECT_INDEX;
+        if (this.word[i].Equals(other.word[i])) return CORRECT_LETTER_CORRECT_INDEX;
         else if (other.word.Contains(this.word[i])) return CORRECT_LETTER_WRONG_INDEX;
         return INCORRECT_LETTER;
     }
